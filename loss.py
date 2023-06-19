db7c0f8d08c5675af50e4c3ef5d44234c170cae1
DGMEFG <97904453+DGMEFG@users.noreply.github.com>
2023-06-19 20:23:17 +0800

    Add files via upload

--- loss.py
+++ loss.py
@@ -4,6 +4,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 from torch.distributions import normal
+from utils import *
 
 
 def get_loss(args, cls_num_list, per_cls_weights):
@@ -20,6 +21,8 @@ def get_loss(args, cls_num_list, per_cls_weights):
         criterion = LDAMLoss(cls_num_list=cls_num_list, max_m=0.5, s=30, weight=per_cls_weights).cuda(args.gpu)
     elif args.loss_type == 'GML':
         criterion = GML(cls_num_list).cuda(args.gpu)
+    elif args.loss_type == 'LADE':
+        criterion = LADELoss()
     else:
         raise NotImplementedError(
             "Error:Loss function {} is not implemented! Please re-choose loss type!".format(args.loss_type))
@@ -61,7 +64,7 @@ class FocalLoss(nn.Module):
 
 class LDAMLoss(nn.Module):
 
-    def __init__(self, cls_num_list, max_m=0.5, weight=False, s=30):
+    def __init__(self, cls_num_list, max_m=0.5, weight=None, s=30):
         super(LDAMLoss, self).__init__()
         m_list = 1.0 / np.sqrt(np.sqrt(cls_num_list))
         m_list = m_list * (max_m / np.max(m_list))
@@ -164,4 +167,56 @@ class GML(nn.Module):
 
         loss = torch.pow(mean_prob_sum, 1.0 / self.p)
 
+        return loss
+
+"""Copyright (c) Hyperconnect, Inc. and its affiliates.
+All rights reserved.
+"""
+
+class LADELoss(nn.Module):
+    def __init__(self, num_classes=10, img_max=500, prior=0.1, prior_txt=None, remine_lambda=0.1):
+        super().__init__()
+        if img_max is not None or prior_txt is not None:
+            self.img_num_per_cls = calculate_prior(num_classes, img_max, prior, prior_txt, return_num=True).float()
+            if torch.cuda.is_available():
+                self.img_num_per_cls = self.img_num_per_cls.cuda()
+            self.prior = self.img_num_per_cls / self.img_num_per_cls.sum()
+        else:
+            self.prior = None
+
+        self.balanced_prior = torch.tensor(1. / num_classes).float()
+
+        self.remine_lambda = remine_lambda
+
+        self.num_classes = num_classes
+        self.cls_weight = (self.img_num_per_cls.float() / torch.sum(self.img_num_per_cls.float()))
+
+        if torch.cuda.is_available():
+            self.balanced_prior = self.balanced_prior.cuda()
+            self.cls_weight = self.cls_weight.cuda()
+    def mine_lower_bound(self, x_p, x_q, num_samples_per_cls):
+        N = x_p.size(-1)
+        first_term = torch.sum(x_p, -1) / (num_samples_per_cls + 1e-8)
+        second_term = torch.logsumexp(x_q, -1) - np.log(N)
+
+        return first_term - second_term, first_term, second_term
+
+    def remine_lower_bound(self, x_p, x_q, num_samples_per_cls):
+        loss, first_term, second_term = self.mine_lower_bound(x_p, x_q, num_samples_per_cls)
+        reg = (second_term ** 2) * self.remine_lambda
+        return loss - reg, first_term, second_term
+
+    def forward(self, y_pred, target, q_pred=None,curr=None):
+        """
+        y_pred: N x C
+        target: N
+        """
+        y_pred = y_pred['score']
+        per_cls_pred_spread = y_pred.T * (target == torch.arange(0, self.num_classes).view(-1, 1).type_as(target))  # C x N
+        pred_spread = (y_pred - torch.log(self.prior + 1e-9) + torch.log(self.balanced_prior + 1e-9)).T  # C x N
+
+        num_samples_per_cls = torch.sum(target == torch.arange(0, self.num_classes).view(-1, 1).type_as(target), -1).float()  # C
+        estim_loss, first_term, second_term = self.remine_lower_bound(per_cls_pred_spread, pred_spread, num_samples_per_cls)
+
+        loss = -torch.sum(estim_loss * self.cls_weight)
         return loss
